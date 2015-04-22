62db6793970cbce29324e45bc696dea2fa07e03a
Matthew Beddoes <matty.beds@red-dragon-security.com>
2015-04-22 04:21:00 +0100

    Update Wireless_Probe_Fuzzer.py

--- Wireless_Probe_Fuzzer.py
+++ Wireless_Probe_Fuzzer.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python2
 
+import threading
+import string
 from scapy.all import *
 
 logfile = "wifiprobe.txt"
 interface = "mon0"
-
+		
 def banner():
   print("WifiProbe-FUZZ -- v1.1 (Red Dragon Productions)")
   print("")
@@ -17,37 +19,56 @@ def banner():
   print("python2 wifi_probe.py")
 
 banner()
-target = raw_input("SSID> ")
-for i in range(1,256):
-  buffer_overflow = ["00"*i,"FF"*i]
-  exploits = [buffer_overflow]
-  for x in exploits[:]:
-    tofuzz = [
-    "Dot11Elt(ID=x,info='00')", 
-    "Dot11Elt(ID='SSID', len=x,info='00')", 
-    "Dot11Elt(ID='SSID', len=9,info=x)", 
-    "Dot11Elt(ID='RATES', info=x)", 
-    "Dot11Elt(ID='EXT RATES ',info=x)",  
-    "Dot11Elt(ID='DS PARAM', info=x)",  
-    "Dot11Elt(ID='COUNTRY', info=x)", 
-    "Dot11Elt(ID='REQUEST',info=x)", 
-    "Dot11Elt(ID='CHALLENGE TEXT ',info=x)", 
-    "Dot11Elt(ID='POWER CONSTRAINT',info=x)", 
-    "Dot11Elt(ID='POWER CAPAB ',info=x)", 
-    "Dot11Elt(ID='CHANNELS',info=x)", 
-    "Dot11Elt(ID='ERP INFO',info=x)", 
-    "Dot11Elt(ID='ERP NONERP PRESENT',info=x)", 
-    "Dot11Elt(ID='CHANNELS CHANNEL BAND',info=x)", 
-    "Dot11Elt(ID='ERP BARKER LONG',info=x)", 
-    "Dot11Elt(ID='RSN',info=x)", 
-    "Dot11Elt(ID='VENDOR',info=x)", 
-    "Dot11Elt(ID='COUNTRY TRIPLET',info=x)", 
-    "Dot11Elt(ID='COUNTRY BAND TRIPLET',info=x)", 
-    "Dot11Elt(ID='COUNTRY EXT TRIPLET',info=x)"]
-    for a in tofuzz[:]:
-      resp = hexdump(srp(
-        RadioTap()/
-        Dot11(type=0,subtype=0100,addr2=target)/
-        Dot11ProbeReq()/
-        a,timeout=4))
-      print(resp)
+targets = raw_input("SSID> ")
+
+def engine():
+  for i in range(1,256):
+    bonull = ["00"*i]
+    bofull = ["FF"*i]
+    exploits = [bonull,bofull]
+    for x in exploits[:]:
+      tofuzz = [
+        "Dot11Elt(ID="+str(x)+",info='00')", 
+        "Dot11Elt(ID='SSID', len="+str(x)+",info='00')", 
+        "Dot11Elt(ID='SSID', len=9,info="+str(x)+")", 
+        "Dot11Elt(ID='RATES', info="+str(x)+")", 
+        "Dot11Elt(ID='EXT RATES ',info="+str(x)+")",  
+        "Dot11Elt(ID='DS PARAM', info="+str(x)+")",  
+        "Dot11Elt(ID='COUNTRY', info="+str(x)+")", 
+        "Dot11Elt(ID='REQUEST',info="+str(x)+")", 
+        "Dot11Elt(ID='CHALLENGE TEXT ',info="+str(x)+")", 
+        "Dot11Elt(ID='POWER CONSTRAINT',info="+str(x)+")", 
+        "Dot11Elt(ID='POWER CAPAB ',info="+str(x)+")", 
+        "Dot11Elt(ID='CHANNELS',info="+str(x)+")", 
+        "Dot11Elt(ID='ERP INFO',info="+str(x)+")", 
+        "Dot11Elt(ID='ERP NONERP PRESENT',info="+str(x)+")", 
+        "Dot11Elt(ID='CHANNELS CHANNEL BAND',info="+str(x)+")", 
+        "Dot11Elt(ID='ERP BARKER LONG',info="+str(x)+")", 
+        "Dot11Elt(ID='RSN',info="+str(x)+")", 
+        "Dot11Elt(ID='VENDOR',info="+str(x)+")", 
+        "Dot11Elt(ID='COUNTRY TRIPLET',info="+str(x)+")", 
+        "Dot11Elt(ID='COUNTRY BAND TRIPLET',info="+str(x)+")", 
+        "Dot11Elt(ID='COUNTRY EXT TRIPLET',info="+str(x)+")"]
+      for a in tofuzz[:]:
+        resp = srp(
+          RadioTap()/
+          Dot11(type=0,subtype=0100,addr2=targets)/
+          Dot11ProbeReq()/
+          a,timeout=4)
+
+#Threads
+
+def getresp(p):
+  logfile = open("wifiprobe.txt","a")
+  dframe = (4,5)
+  if p.haslayer(Dot11):
+    if p.addr1 == targets:
+      if p.type == 0 and p.subtype in dframe:
+        logfile.write(hexdump(p)+"/n/n")
+        print(hexdump(p)+"/n/n")
+
+t = threading.Thread(target = engine)
+t.start()
+while True:
+  logfile = open("wifiprobe.txt","a")
+  sniff(prn=getresp, count=1))
